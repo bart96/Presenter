0212f514533f5353aa699bb3d96e1b30305db6d8
Marcel Birkholz <birkholz96@gmx.de>
2020-09-02 22:54:53 +0200

    Added sql install script (DDL)

---
+++ install.sql
@@ -0,0 +1,43 @@
+
+START TRANSACTION;
+
+CREATE TABLE `account` (
+  `license` int(11) NOT NULL,
+  `mail` varchar(200) NOT NULL,
+  `song_index` int(11) NOT NULL DEFAULT '0',
+  `lastactivity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `blocks` (
+  `account` int(11) NOT NULL,
+  `songnumber` int(11) NOT NULL,
+  `type` varchar(60) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `songs` (
+  `account` int(11) NOT NULL,
+  `songnumber` int(11) NOT NULL,
+  `title` varchar(300) NOT NULL,
+  `initialOrder` varchar(150) NOT NULL,
+  `order` varchar(300) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`license`);
+
+
+ALTER TABLE `blocks`
+  ADD KEY `fk_song` (`account`,`songnumber`);
+ALTER TABLE `blocks` ADD FULLTEXT KEY `text` (`text`);
+
+
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`account`,`songnumber`);
+ALTER TABLE `songs` ADD FULLTEXT KEY `title` (`title`);
+
+COMMIT;
